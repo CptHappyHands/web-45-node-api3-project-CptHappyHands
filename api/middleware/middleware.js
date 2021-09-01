996340883089788713afce0edef3793dc1bb05e3
Andrew Cummings <andrew.amalee@gmail.com>
2021-09-01 15:37:43 -0700

    first push. 4/25 passing

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,40 @@
+const User = require("../users/users-model");
+
+// const Post = require("../posts/posts-model");
+
 function logger(req, res, next) {
-  // DO YOUR MAGIC
+  console.log(`[${new Date().toISOString()}]${req.method} to ${req.url}`);
+  next();
 }
 
 function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+  const { id } = req.params;
+  User.getById(id).then((possibleUser) => {
+    if (possibleUser) {
+      req.id = possibleUser;
+      next();
+    } else {
+      next({ message: "user not found", status: 404 });
+    }
+  });
+  next();
 }
 
 function validateUser(req, res, next) {
-  // DO YOUR MAGIC
+  if (!req.body.name) {
+    res.status(400).json({ message: "missing required name field" });
+  } else {
+    next();
+  }
 }
 
 function validatePost(req, res, next) {
-  // DO YOUR MAGIC
+  if (!req.body.text) {
+    res.status(400).json({ message: "missing required text field" });
+  } else {
+    next();
+  }
 }
 
 // do not forget to expose these functions to other modules
+module.exports = { logger, validateUserId, validateUser, validatePost };
